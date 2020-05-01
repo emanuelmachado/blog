15c3994e78855d9db866e472ecec74afb2035120
egodoymachado <egodoymachado@hotmail.com>
2020-04-30 22:17:09 -0300

    Estrutura inicial do projeto

---
+++ application/views/contact.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Página - Contato!</h1>
+
+	<div id="body">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem massa, sollicitudin vitae ultrices ut, auctor ac orci. Fusce varius mauris eu finibus efficitur. Pellentesque tincidunt, nunc in tempus auctor, eros orci ornare dui, ut auctor neque dui sed velit. Mauris nunc libero, fermentum eget elementum in, eleifend in dui. Maecenas commodo tellus vitae ornare pellentesque. Ut finibus lacus a neque pulvinar, ac dictum velit interdum. Vivamus scelerisque tristique dapibus.</p>
+
+        <p>Maecenas a dictum est, eu ultrices ligula. Sed vitae rutrum lectus. Vivamus tempor facilisis commodo. Aliquam euismod hendrerit sapien porttitor ultrices. Aenean efficitur volutpat lacus vel efficitur. Praesent vitae mauris sit amet velit iaculis molestie rhoncus ac nunc. Aliquam interdum enim id faucibus vulputate. Sed augue nunc, laoreet eget dolor nec, suscipit interdum nisl. Vestibulum consectetur tristique nibh, in auctor orci maximus et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam consequat lacinia dui, eget accumsan neque convallis vitae.</p>
+
+        <p>Quisque nec massa lacus. Curabitur in consequat nibh, sit amet tincidunt risus. Praesent eu justo scelerisque, feugiat odio sed, dictum est. Sed quis sem a quam congue aliquet. In neque velit, rutrum at augue ut, mollis dignissim nisl. Aenean a tempor augue, non feugiat lorem. Donec sagittis, libero et lobortis iaculis, leo eros feugiat nisi, hendrerit ultricies odio felis quis ipsum. In hac habitasse platea dictumst. Maecenas ornare hendrerit leo eget dignissim. Sed sed ipsum a nisl feugiat volutpat vel eget nunc. Vestibulum accumsan, nunc id pharetra maximus, tellus nunc maximus felis, in viverra arcu ipsum accumsan tellus. Morbi accumsan lacus a dolor porttitor, eu sollicitudin justo efficitur.</p>
+
+        <p>Quisque efficitur convallis mi in molestie. Morbi vitae mi quis mauris faucibus dignissim. Praesent erat velit, sodales non faucibus sed, sodales id est. Donec consequat mauris eget lacus tristique, a congue lacus dapibus. Aenean ac odio nec elit sagittis tincidunt vel at elit. Nullam eget arcu sapien. Sed iaculis maximus arcu et viverra. Proin vel varius dui, non ultrices augue. Nulla mollis odio vitae eros pharetra, non gravida purus aliquam. Donec a placerat erat. Morbi eget feugiat mauris. Duis tincidunt eros quam, ut tempor arcu facilisis in. Vestibulum posuere, diam sed fringilla mattis, felis nisi posuere arcu, vitae congue lectus sem a lorem.</p>
+
+        <p>Pellentesque egestas tellus ut tortor mollis, nec gravida erat tempor. In vel mauris non elit vestibulum aliquam in vitae diam. Cras id nulla elit. Sed in fermentum ex. Integer non turpis id ipsum ornare feugiat. In nec vestibulum nulla, et aliquet turpis. Donec mollis nulla quam.</p>
+	</div>
+</div>
+
+</body>
+</html>
